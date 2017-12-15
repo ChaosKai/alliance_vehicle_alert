0920d4ab172e0a89e106fce9b9f42f8a0db64363
ChaosKai <kai-waggeling@web.de>
2017-12-15 03:40:59 +0100

    Create vehicle-alert.js

---
+++ module/vehicle-alert.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function()
+{
+    MissionID = $("#mission_reply_mission_id").val();
+    setInterval( collectMissions, 1000 );
+});
+
+var MissionID;
+var MissionsReady;
+
+function checkAutomaticAlert()
+{
+    MissionsReady = JSON.parse(localStorage.getItem("AllianceVehicleAlert-MissionsReady"));
+    
+    if( $.inArray( MissionID, MissionsReady ) )
+    {
+        alertFirstVehicle();
+    }
+}
+
+function alertFirstVehicle()
+{
+    var TimeLeft   = 0;
+    var FoundVehicle = false;
+    var TimeValues = $("#mission_countdown_" + MissionID).text().split(":");
+
+    for(i = 0; i < TimeValues.length; i++)
+    {
+         TimeLeft = (TimeLeft * 60) + parseInt(TimeValues[i]);
+    }
+    
+    AllianceVehicleAlert_AllowedVehicleTypes.foreach( function(VehicleType)
+    {
+        
+        $("#vehicle_show_table_body_all").find(".vehicle_select_table_tr").each( function()
+        {
+            var VehicleID = $(this).attr("id").replace("vehicle_element_content_", "");
+            var VehicleDistanceTime = $("#vehicle_sort_" + VehicleID).attr("sortvalue");
+            
+            if( $(this).attr("vehicle_type") == VehicleType && VehicleDistanceTime < TimeLeft && !FoundVehicle )
+            {
+                $("#vehicle_checkbox_" + VehicleID).click();
+                FoundVehicle = true;
+                
+                MissionsReady.splice( MissionsReady.indexOf(MissionID), 1 );
+                localStorage.setItem( "AllianceVehicleAlert-MissionsReady", JSON.stringify(MissionsReady) );
+                
+                $("#mission_alarm_btn").first().click();
+            }
+        });
+        
+    });
+}
